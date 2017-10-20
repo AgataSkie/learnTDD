702a9a5abd0aa7b15bc4fd4847f121b3ccad019e
Agata Skierska <askierska@togetherdata.com>
2017-10-20 16:32:05 +0200

    added views tests and views, corrected spelling mistake

--- intro/forms.py
+++ intro/forms.py
@@ -7,3 +7,9 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = models.Comment
         exclude = []
+
+    def clean_text(self):
+        data = self.cleaned_data.get('text')
+        if len(data) <= 1:
+            raise forms.ValidationError("Your comment is too short")
+        return data
--- tests/test_forms.py
+++ tests/test_forms.py
@@ -3,7 +3,6 @@ import pytest
 from mixer.backend.django import mixer
 
 from intro import forms
-from intro.models import Person
 
 pytestmark = pytest.mark.django_db
 
@@ -13,7 +12,14 @@ class TestPostForm:
         assert form.is_valid() is False, 'required fields are blank, should be invalid'
         assert 'person' in form.errors
 
+    def test_form_too_short_text(self):
+        person = mixer.blend('intro.Person')
+        form = forms.CommentForm(data={'person': person.pk, 'text': '.'})
+        assert form.is_valid() is False, 'Text should be longer than 1 char'
+
     def test_correct_form(self):
         person = mixer.blend('intro.Person')
-        form = forms.CommentForm(data={'person':person.pk, 'text':'Some comment text'})
-        assert form.is_valid() == True, 'filled in form should be valid'
+        form = forms.CommentForm(
+            data={'person': person.pk, 'text': 'Some comment text'}
+        )
+        assert form.is_valid() is True, 'filled in form should be valid'
